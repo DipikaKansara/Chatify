c17df4e1cccd2a2ecdddbf997027c490e5b0fac5
Dipika Kansara <68623123+DipikaKansara@users.noreply.github.com>
2021-08-11 10:05:55 +1000

    Initial Commit

---
+++ Chatify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chatify
+//
+//  Created by Dipika Kansara on 11/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
